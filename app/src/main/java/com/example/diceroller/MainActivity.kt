c08a62101cf81d4e95262d199461c5a89ef25f4f
Soumyajoy Das <70829126+soumyajoy01@users.noreply.github.com>
2021-07-21 08:43:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        Get the Button view from the layout and assign a click
+//        Listen to it
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{ rollDice()}
+    }
+
+    /*
+    * Click listener for the Roll Button.
+    * */
+
+
+    private fun rollDice(){
+//        Toast.makeText(this, "button clicked",
+//            Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
+    }
+
+}
